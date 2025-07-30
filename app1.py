cbf04b9c91dae4f72e1f2f4f81a6095ae1590a26
bhavyanagarapu123-lgtm <nagarapubhavyayedukondalu@gmail.com>
2025-07-30 14:09:04 +0530

    adding files

---
+++ app1.py
@@ -0,0 +1,12 @@
+name="MLEWC"
+number=10.0
+isOn=True
+Letter="P"
+#Arithmetic - +-*/%
+#logical -and,or,not
+#comparision- <,>!=,<=,>=
+#assignment- =,+=,-=,*=,/=
+if (number>5):
+    print (name)
+else:
+    print(isOn)
---
+++ day2.py
@@ -0,0 +1,6 @@
+num = 56
+# Check if the number is even or odd using modulus operator
+if num % 2 == 0:
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
